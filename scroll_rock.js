5c23d11c3e0106278489ad53300cc370fec6a7a7
‘keinn51’ <soogood97@gmail.com>
2022-09-26 17:55:43 +0900

    WIP: 자바스크립트 함수 리팩토링

--- scroll_rock.js
+++ scroll_rock.js
@@ -1,13 +1,11 @@
-function getSidebarPosition() {
-    $(window).resize(
-        function () {
-            let leftSidebarWidth = document.getElementsByClassName('sidebar-left')[0];
-            console.log(leftSidebarWidth.width)
-        }
-    )
+function getAppearingOpacityByScroll(startScroll, scrollWidth) {
+    if (scrollY > startScroll) return (scrollY - startScroll) / scrollWidth
+    else return 0
 }
 
-getSidebarPosition()
+function getPositionByScroll(startScroll, scrollWidth,) {
+
+}
 
 function scollShow() {
     const animationStartScrollY = 500;
@@ -15,39 +13,39 @@ function scollShow() {
     const sidebarStartScrollY = 1100;
     const imageStartScrollY = 1700;
     const animationIntervalHeight = imageStartScrollY - sidebarStartScrollY
-    const slideWidht = 50;
 
     $(document).on('scroll touchmove mousewheel', function () {
-        let leftSidebarWidth = document.getElementsByClassName('sidebar-left')[0].getBoundingClientRect().width;
-        let topSidebarHeight = document.getElementsByClassName('sidebar-top')[0].getBoundingClientRect().height;
-        let mainImageWidth = document.getElementsByClassName('main-image')[0].getBoundingClientRect().width;
-        let mainImageHeight = document.getElementsByClassName('main-image')[0].getBoundingClientRect().height;
+        let leftSidebarWidth = document.getElementsByClassName('main-preview-sidebar-left')[0].getBoundingClientRect().width;
+        let topSidebarHeight = document.getElementsByClassName('main-preview-sidebar-top')[0].getBoundingClientRect().height;
+        let mainImageWidth = document.getElementsByClassName('main-preview-image')[0].getBoundingClientRect().width;
+        let mainImageHeight = document.getElementsByClassName('main-preview-image')[0].getBoundingClientRect().height;
+        let slideWidht = leftSidebarWidth;
         console.log(scrollY)
-
-        $('.sidebar-left').css({ 'left': -leftSidebarWidth, 'height': mainImageHeight });
-        $('.sidebar-top').css({ 'top': -topSidebarHeight, 'width': mainImageWidth + leftSidebarWidth });
+        $('.main-preview-sidebar-left').css({ 'left': -leftSidebarWidth, 'height': mainImageHeight });
+        $('.main-preview-sidebar-top').css({ 'top': -topSidebarHeight, 'width': mainImageWidth + leftSidebarWidth });
+        $('.main-preview-contents').css({ 'height': mainImageHeight });
 
         if (scrollY > animationStartScrollY && scrollY < animationEndScrollY) {
-            $('.position-box').addClass('fixed-image');
+            $('.main-preview-contents').addClass('fixed-image');
         } else {
-            $('.position-box').removeClass('fixed-image');
+            $('.main-preview-contents').removeClass('fixed-image');
         }
 
         if (scrollY >= animationEndScrollY) {
-            $('.position-box').addClass('abosulute-bottom-100');
+            $('.main-preview-contents').addClass('abosulute-bottom-100');
         } else {
-            $('.position-box').removeClass('abosulute-bottom-100');
+            $('.main-preview-contents').removeClass('abosulute-bottom-100');
         }
 
-        $('.sidebar-left').css({ 'opacity': scrollY > sidebarStartScrollY ? (scrollY - sidebarStartScrollY) / animationIntervalHeight : 0 });
-        $('.sidebar-top').css({ 'opacity': scrollY > sidebarStartScrollY ? (scrollY - sidebarStartScrollY) / animationIntervalHeight : 0 });
-        $('.left-image').css({ 'opacity': scrollY > imageStartScrollY ? (scrollY - imageStartScrollY) / animationIntervalHeight : 0 });
-        $('.right-image').css({ 'opacity': scrollY > imageStartScrollY ? (scrollY - imageStartScrollY) / animationIntervalHeight : 0 });
+        $('.main-preview-sidebar-left').css({ 'opacity': getAppearingOpacityByScroll(sidebarStartScrollY, animationIntervalHeight) });
+        $('.main-preview-sidebar-top').css({ 'opacity': getAppearingOpacityByScroll(sidebarStartScrollY, animationIntervalHeight) });
+        $('.main-preview-popup-left').css({ 'opacity': getAppearingOpacityByScroll(imageStartScrollY, animationIntervalHeight) });
+        $('.main-preview-popup-right').css({ 'opacity': getAppearingOpacityByScroll(imageStartScrollY, animationIntervalHeight) });
 
-        $('.sidebar-top').css({ 'left': scrollY > sidebarStartScrollY ? scrollY > imageStartScrollY ? -leftSidebarWidth : -((scrollY - sidebarStartScrollY) * (leftSidebarWidth / animationIntervalHeight)) : 0 });
-        $('.sidebar-left').css({ 'top': scrollY > sidebarStartScrollY ? scrollY > imageStartScrollY ? 0 : (animationIntervalHeight - (scrollY - sidebarStartScrollY)) * (topSidebarHeight / animationIntervalHeight) : slideWidht });
-        $('.left-image').css({ 'bottom': scrollY > imageStartScrollY ? scrollY > animationEndScrollY ? -75 : (animationIntervalHeight - (scrollY - imageStartScrollY)) * (slideWidht / animationIntervalHeight) - 75 : -25 });
-        $('.right-image').css({ 'bottom': scrollY > imageStartScrollY ? scrollY > animationEndScrollY ? slideWidht : (scrollY - imageStartScrollY) * (slideWidht / animationIntervalHeight) : 0 });
+        $('.main-preview-sidebar-top').css({ 'left': scrollY > sidebarStartScrollY ? scrollY > imageStartScrollY ? -slideWidht : -((scrollY - sidebarStartScrollY) * (leftSidebarWidth / animationIntervalHeight)) : 0 });
+        $('.main-preview-sidebar-left').css({ 'top': scrollY > sidebarStartScrollY ? scrollY > imageStartScrollY ? 0 : (animationIntervalHeight - (scrollY - sidebarStartScrollY)) * (topSidebarHeight / animationIntervalHeight) : slideWidht });
+        $('.main-preview-popup-left').css({ 'bottom': scrollY > imageStartScrollY ? scrollY > animationEndScrollY ? -(25 + slideWidht) : (animationIntervalHeight - (scrollY - imageStartScrollY)) * (slideWidht / animationIntervalHeight) - (25 + slideWidht) : -25 });
+        $('.main-preview-popup-right').css({ 'bottom': scrollY > imageStartScrollY ? scrollY > animationEndScrollY ? slideWidht : (scrollY - imageStartScrollY) * (slideWidht / animationIntervalHeight) : 0 });
 
     });
 }
