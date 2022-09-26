2e560d4470739251724566477e7a02456bda3d1e
‘keinn51’ <soogood97@gmail.com>
2022-09-26 14:57:55 +0900

    Image 삽입 후 적용

--- scroll_rock.js
+++ scroll_rock.js
@@ -1,4 +1,13 @@
+function getSidebarPosition() {
+    $(window).resize(
+        function () {
+            let leftSidebarWidth = document.getElementsByClassName('sidebar-left')[0];
+            console.log(leftSidebarWidth.width)
+        }
+    )
+}
 
+getSidebarPosition()
 
 function scollShow() {
     const animationStartScrollY = 500;
@@ -9,14 +18,24 @@ function scollShow() {
     const slideWidht = 50;
 
     $(document).on('scroll touchmove mousewheel', function () {
+        let leftSidebarWidth = document.getElementsByClassName('sidebar-left')[0].width;
+        let leftSidebarHeight = document.getElementsByClassName('sidebar-left')[0].height;
+        let topSidebarWidth = document.getElementsByClassName('sidebar-top')[0].width;
+        let topSidebarHeight = document.getElementsByClassName('sidebar-top')[0].height;
+        let mainImageWidth = document.getElementsByClassName('main-image')[0].width;
+        let mainImageHeight = document.getElementsByClassName('main-image')[0].height;
         console.log(scrollY)
+
+        $('.sidebar-left').css({ 'left': -leftSidebarWidth, 'height': mainImageHeight });
+        $('.sidebar-top').css({ 'top': -topSidebarHeight, 'width': mainImageWidth + leftSidebarWidth });
+
         if (scrollY > animationStartScrollY && scrollY < animationEndScrollY) {
             $('.position-box').addClass('fixed-image');
         } else {
             $('.position-box').removeClass('fixed-image');
         }
 
-        if (scrollY >= 2300) {
+        if (scrollY >= animationEndScrollY) {
             $('.position-box').addClass('abosulute-bottom-100');
         } else {
             $('.position-box').removeClass('abosulute-bottom-100');
@@ -27,8 +46,8 @@ function scollShow() {
         $('.left-image').css({ 'opacity': scrollY > imageStartScrollY ? (scrollY - imageStartScrollY) / animationIntervalHeight : 0 });
         $('.right-image').css({ 'opacity': scrollY > imageStartScrollY ? (scrollY - imageStartScrollY) / animationIntervalHeight : 0 });
 
-        $('.sidebar-top').css({ 'left': scrollY > sidebarStartScrollY ? scrollY > imageStartScrollY ? -slideWidht : -((scrollY - sidebarStartScrollY) / 12) : 0 });
-        $('.sidebar-left').css({ 'top': scrollY > sidebarStartScrollY ? scrollY > imageStartScrollY ? 0 : (animationIntervalHeight - (scrollY - sidebarStartScrollY)) / 12 : slideWidht });
+        $('.sidebar-top').css({ 'left': scrollY > sidebarStartScrollY ? scrollY > imageStartScrollY ? -leftSidebarWidth : -((scrollY - sidebarStartScrollY) * (leftSidebarWidth / animationIntervalHeight)) : 0 });
+        $('.sidebar-left').css({ 'top': scrollY > sidebarStartScrollY ? scrollY > imageStartScrollY ? 0 : (animationIntervalHeight - (scrollY - sidebarStartScrollY)) * (topSidebarHeight / animationIntervalHeight) : slideWidht });
         $('.left-image').css({ 'bottom': scrollY > imageStartScrollY ? scrollY > animationEndScrollY ? -20 : (animationIntervalHeight - (scrollY - imageStartScrollY)) / 12 - 20 : 30 });
         $('.right-image').css({ 'bottom': scrollY > imageStartScrollY ? scrollY > animationEndScrollY ? slideWidht : (scrollY - imageStartScrollY) / 12 : 0 });
 
