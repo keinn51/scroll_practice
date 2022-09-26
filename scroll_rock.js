aeaf6183558da9b455bfb15f3b7139a1dcb315c1
‘keinn51’ <soogood97@gmail.com>
2022-09-26 12:57:24 +0900

    변수로 숫자 설정

--- scroll_rock.js
+++ scroll_rock.js
@@ -3,13 +3,14 @@
 function scollShow() {
     const animationStartScrollY = 500;
     const animationEndScrollY = 2300;
-    const sidebarStartScrollY = 1300;
-    const imageStartScrollY = 1800;
+    const sidebarStartScrollY = 1100;
+    const imageStartScrollY = 1700;
     const animationIntervalHeight = imageStartScrollY - sidebarStartScrollY
+    const slideWidht = 50;
 
-    $(document).on('scroll touchmove mousewheel', function (e) {
+    $(document).on('scroll touchmove mousewheel', function () {
         console.log(scrollY)
-        if (scrollY > 500 && scrollY < 2300) {
+        if (scrollY > animationStartScrollY && scrollY < animationEndScrollY) {
             $('.position-box').addClass('fixed-image');
         } else {
             $('.position-box').removeClass('fixed-image');
@@ -26,10 +27,10 @@ function scollShow() {
         $('.left-image').css({ 'opacity': scrollY > imageStartScrollY ? (scrollY - imageStartScrollY) / animationIntervalHeight : 0 });
         $('.right-image').css({ 'opacity': scrollY > imageStartScrollY ? (scrollY - imageStartScrollY) / animationIntervalHeight : 0 });
 
-        $('.sidebar-top').css({ 'left': scrollY > sidebarStartScrollY ? scrollY > imageStartScrollY ? -50 : -((scrollY - sidebarStartScrollY) / 10) : 0 });
-        $('.sidebar-left').css({ 'top': scrollY > sidebarStartScrollY ? scrollY > imageStartScrollY ? 0 : (500 - (scrollY - sidebarStartScrollY)) / 10 : 50 });
-        $('.left-image').css({ 'opacity': scrollY > imageStartScrollY ? (scrollY - imageStartScrollY) / animationIntervalHeight : 0 });
-        $('.right-image').css({ 'opacity': scrollY > imageStartScrollY ? (scrollY - imageStartScrollY) / animationIntervalHeight : 0 });
+        $('.sidebar-top').css({ 'left': scrollY > sidebarStartScrollY ? scrollY > imageStartScrollY ? -slideWidht : -((scrollY - sidebarStartScrollY) / 12) : 0 });
+        $('.sidebar-left').css({ 'top': scrollY > sidebarStartScrollY ? scrollY > imageStartScrollY ? 0 : (animationIntervalHeight - (scrollY - sidebarStartScrollY)) / 12 : slideWidht });
+        $('.left-image').css({ 'bottom': scrollY > imageStartScrollY ? scrollY > animationEndScrollY ? -20 : (animationIntervalHeight - (scrollY - imageStartScrollY)) / 12 - 20 : 30 });
+        $('.right-image').css({ 'bottom': scrollY > imageStartScrollY ? scrollY > animationEndScrollY ? slideWidht : (scrollY - imageStartScrollY) / 12 : 0 });
 
     });
 }
